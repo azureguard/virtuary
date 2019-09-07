bd3cb4e07520ba913aa852bab8c172a224ec70b9
Maurice <Maurice_tikus@hotmail.com>
2019-09-07 17:45:10 +1000

    Implemented Forgot Password Layout

--- app/src/main/java/com/virtuary/app/screens/forgotPassword/ForgotPasswordFragment.kt
+++ app/src/main/java/com/virtuary/app/screens/forgotPassword/ForgotPasswordFragment.kt
@@ -1,40 +1,41 @@
-package com.virtuary.app.screens.login
+package com.virtuary.app.screens.forgotPassword
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.findNavController
 import com.virtuary.app.R
-import com.virtuary.app.databinding.FragmentLoginBinding
+import com.virtuary.app.databinding.FragmentForgotPasswordBinding
 
 
 /**
  * Fragment for the starting screen of the app
  */
-class LoginFragment : Fragment() {
+class ForgotPasswordFragment : Fragment() {
 
-    private lateinit var viewModel: LoginViewModel
+    private lateinit var viewModel: ForgotPasswordViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_login, container, false)
+        val binding: FragmentForgotPasswordBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_forgot_password, container, false)
+
+        // Set up button
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // Get the viewmodel
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(ForgotPasswordViewModel::class.java)
 
         // Set the viewmodel for databinding - this allows the bound layout access to all of the
         // data in the VieWModel
-        binding.loginViewModel = viewModel
+        binding.forgotPasswordViewModel = viewModel
 
-        binding.forgotPassword.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToForgotPasswordFragment())
-        }
         return binding.root
     }
 }
