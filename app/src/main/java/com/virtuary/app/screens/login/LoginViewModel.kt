3ad0f0e9c7221d92e6ab474de09b92884adf4cbc
Maurice <Maurice_tikus@hotmail.com>
2019-09-09 15:02:27 +1000

    Removed Unnecessary Log Import

--- app/src/main/java/com/virtuary/app/screens/login/LoginViewModel.kt
+++ app/src/main/java/com/virtuary/app/screens/login/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.virtuary.app.screens.login
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
