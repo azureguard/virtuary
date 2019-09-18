2bc6b31cb3c3f6d5ce32e95a968d2172dda50c57
Daryl Wong <daryl.jl.wong@gmail.com>
2019-09-18 20:51:30 +1000

    Fixed and cleaned up databinding

--- app/src/main/java/com/virtuary/app/screens/manageAccount/EditProfileDetails.kt
+++ app/src/main/java/com/virtuary/app/screens/manageAccount/EditProfileDetails.kt
@@ -1,29 +1,22 @@
 package com.virtuary.app.screens.manageAccount
 
-import androidx.lifecycle.ViewModelProviders
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import com.virtuary.app.R
-import android.content.Context
-import android.content.DialogInterface
-import android.text.SpannableStringBuilder
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.virtuary.app.R
 import com.virtuary.app.databinding.FragmentEditProfileDetailsBinding
 
 
 class EditProfileDetails : Fragment() {
-
-    companion object {
-        fun newInstance() = EditProfileDetails()
-    }
-
-    private lateinit var viewModel: EditProfileDetailsViewModel
+    private val viewModel by viewModels<EditProfileDetailsViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,46 +25,50 @@ class EditProfileDetails : Fragment() {
         val binding: FragmentEditProfileDetailsBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_edit_profile_details, container, false
         )
+
+        binding.editProfileViewModel = viewModel
         binding.frameName.setOnClickListener {
             createDialog(context!!, resources.getString(R.string.name))
         }
         binding.frameEmail.setOnClickListener {
             createDialog(context!!, resources.getString(R.string.email))
         }
-        return binding.root
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(EditProfileDetailsViewModel::class.java)
-        // TODO: Use the ViewModel
+        return binding.root
     }
 
     private fun createDialog(context: Context, title: String) {
         val builder = AlertDialog.Builder(context)
-        builder.setTitle(title)
-
         val viewInflated = LayoutInflater.from(context)
             .inflate(R.layout.dialog_edit_profile, view as ViewGroup?, false)
 
         // Set up the input
         val input = viewInflated.findViewById(R.id.input) as EditText
         when (title) {
-            resources.getString(R.string.name) -> input.text = SpannableStringBuilder(viewModel.name.get()!!)
-            resources.getString(R.string.email) -> input.text = SpannableStringBuilder(viewModel.email.get()!!)
+            resources.getString(R.string.name) -> input.text =
+                SpannableStringBuilder(viewModel.name.get())
+            resources.getString(R.string.email) -> input.text =
+                SpannableStringBuilder(viewModel.email.get())
         }
 
-        builder.setView(viewInflated)
-
-        // Set up the buttons
-        builder.setPositiveButton(android.R.string.ok
-        ) { dialog, which ->
-            dialog.dismiss()
+        // Create the alert dialog
+        builder.apply {
+            setTitle(title)
+            setView(viewInflated)
+            setPositiveButton(
+                android.R.string.ok
+            ) { dialog, _ ->
+                when (title) {
+                    resources.getString(R.string.name) -> viewModel.name.set(input.text.toString())
+                    resources.getString(R.string.email) -> viewModel.email.set(input.text.toString())
+                }
+                dialog.dismiss()
+            }
+            setNegativeButton(
+                android.R.string.cancel
+            ) { dialog, _ -> dialog.cancel() }
         }
-        builder.setNegativeButton(android.R.string.cancel
-        ) { dialog, which -> dialog.cancel() }
 
         builder.show()
     }
-
 }
