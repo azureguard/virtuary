27bd273e95c4235e52906003c2939343a4b05734
Sii Kim Lau <s.lau22@student.unimelb.edu.au>
2019-09-20 16:35:18 +1000

    added add item floating action button and navigate to add item fragment

--- app/src/main/java/com/virtuary/app/screens/home/HomeFragment.kt
+++ app/src/main/java/com/virtuary/app/screens/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.virtuary.app.R
 import com.virtuary.app.databinding.FragmentHomeBinding
 
@@ -36,6 +37,10 @@ class HomeFragment : Fragment() {
             homeViewModel.artifactsLocation,
             this
         )
+
+        binding.addItemButton.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToAddItemFragment())
+        }
 
         return binding.root
     }
