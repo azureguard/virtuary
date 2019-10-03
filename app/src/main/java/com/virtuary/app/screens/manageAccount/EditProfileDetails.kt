c6b796eb9563933c1e4d9461f9b935b59a97f020
Daryl Wong <daryl.jl.wong@gmail.com>
2019-10-03 15:37:33 +1000

    Switched dialog boxes to material components

--- app/src/main/java/com/virtuary/app/screens/manageAccount/EditProfileDetails.kt
+++ app/src/main/java/com/virtuary/app/screens/manageAccount/EditProfileDetails.kt
@@ -7,10 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.virtuary.app.R
 import com.virtuary.app.databinding.FragmentEditProfileDetailsBinding
 
@@ -38,7 +38,7 @@ class EditProfileDetails : Fragment() {
     }
 
     private fun createDialog(context: Context, title: String) {
-        val builder = AlertDialog.Builder(context)
+        val builder = MaterialAlertDialogBuilder(context)
         val viewInflated = LayoutInflater.from(context)
             .inflate(R.layout.dialog_edit_profile, view as ViewGroup?, false)
 
