b7eb9914e7d3d2e47c9fe0dc68737e2fe5f3b2d2
Michelle Anggana <michelle.anggana266@gmail.com>
2019-10-07 23:40:30 +1100

    Delete Item connection

--- app/src/main/java/com/virtuary/app/screens/item/ItemFragment.kt
+++ app/src/main/java/com/virtuary/app/screens/item/ItemFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
@@ -18,6 +19,7 @@ class ItemFragment : Fragment() {
 
     // argument got from navigation action
     private val args: ItemFragmentArgs by navArgs()
+    internal val viewModel by viewModels<ItemViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,10 +39,7 @@ class ItemFragment : Fragment() {
         // set options menu
         setHasOptionsMenu(true)
 
-        // get the item view model
         // assign for databinding so the data in view model can be accessed
-        val viewModel: ItemViewModel = ViewModelProviders.of(this).get(ItemViewModel::class.java)
-
         binding.itemViewModel = viewModel
         viewModel.addRelatedTo(args.item.relations)
         // can observe LiveData updates
@@ -95,8 +94,8 @@ class ItemFragment : Fragment() {
                 )
             }
             R.id.nav_remove_item -> {
-                // TODO: change remove button behaviour
-                Log.i("RemoveItem", "Remove Item Menu Button clicked")
+                viewModel.deleteItem(args.item)
+                findNavController().navigate(ItemFragmentDirections.actionGlobalHomeFragment())
             }
         }
 
--- app/src/main/java/com/virtuary/app/screens/item/ItemViewModel.kt
+++ app/src/main/java/com/virtuary/app/screens/item/ItemViewModel.kt
@@ -3,8 +3,11 @@ package com.virtuary.app.screens.item
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.virtuary.app.firebase.FirestoreRepository
+import com.virtuary.app.firebase.Item
 
 class ItemViewModel : ViewModel() {
+    private val repository: FirestoreRepository = FirestoreRepository()
     private val _relatedTo = MutableLiveData<MutableList<String>>()
     val relatedTo: LiveData<MutableList<String>>
         get() = _relatedTo
@@ -13,7 +16,11 @@ class ItemViewModel : ViewModel() {
         _relatedTo.value = mutableListOf()
     }
 
-    fun addRelatedTo(relations : List<String>?) {
+    fun addRelatedTo(relations: List<String>?) {
         _relatedTo.value = relations?.toMutableList() ?: mutableListOf()
+    }
+
+    fun deleteItem(item: Item) {
+        repository.deleteItem(item)
     }
 }
