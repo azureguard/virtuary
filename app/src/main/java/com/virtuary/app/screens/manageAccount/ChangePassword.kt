957823f8ebfbf399c01c4c5bddeae03053304cc0
Daryl Wong <daryl.jl.wong@gmail.com>
2019-10-15 13:28:15 +1100

    Implemented password change

--- app/src/main/java/com/virtuary/app/screens/manageAccount/ChangePassword.kt
+++ app/src/main/java/com/virtuary/app/screens/manageAccount/ChangePassword.kt
@@ -1,33 +1,92 @@
 package com.virtuary.app.screens.manageAccount
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.virtuary.app.R
+import com.virtuary.app.databinding.FragmentChangePasswordBinding
+import com.virtuary.app.util.hideKeyboard
 
 class ChangePassword : Fragment() {
 
-    companion object {
-        fun newInstance() = ChangePassword()
-    }
-
-    private lateinit var viewModel: ChangePasswordViewModel
+    private val viewModel: ChangePasswordViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_change_password, container, false)
-    }
+        val binding: FragmentChangePasswordBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_change_password, container, false
+        )
+        binding.changePasswordViewModel = viewModel
+        binding.lifecycleOwner = this
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(ChangePasswordViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        viewModel.invalidCurrentPassword.observe(this, Observer { invalid ->
+            if (invalid) {
+                binding.currentPasswordText.error = getString(R.string.error_invalid_password)
+                binding.currentPasswordText.isErrorEnabled = true
+            } else {
+                binding.currentPasswordText.isErrorEnabled = false
+            }
+        })
 
+        binding.currentPasswordEdit.doAfterTextChanged {
+            binding.currentPasswordText.isErrorEnabled = false
+        }
+
+        viewModel.invalidNewPassword.observe(this, Observer { invalid ->
+            if (invalid) {
+                binding.newPasswordText.error = getString(R.string.error_password_short)
+                binding.newPasswordText.isErrorEnabled = true
+            } else {
+                binding.newPasswordText.isErrorEnabled = false
+            }
+        })
+
+        binding.newPasswordEdit.doAfterTextChanged {
+            binding.newPasswordText.isErrorEnabled = false
+        }
+
+        binding.newPasswordEdit.setOnKeyListener { v, keyCode, event ->
+            if (keyCode == EditorInfo.IME_ACTION_DONE || keyCode == KeyEvent.KEYCODE_ENTER
+                && event.action == KeyEvent.ACTION_UP
+            ) {
+                v.clearFocus()
+                viewModel.onClick()
+                return@setOnKeyListener true
+            }
+            return@setOnKeyListener false
+        }
+
+        viewModel.inProgress.observe(this, Observer {
+            if (it) {
+                binding.changePasswordBtn.isEnabled = false
+                binding.progressBar.visibility = View.VISIBLE
+                hideKeyboard()
+            } else {
+                binding.changePasswordBtn.isEnabled = true
+                binding.progressBar.visibility = View.GONE
+            }
+        })
+
+        viewModel.isSuccess.observe(this, Observer {
+            if (it) {
+                findNavController().navigateUp()
+                Toast.makeText(context, "Password change successful", Toast.LENGTH_LONG).show()
+            }
+        })
+
+        return binding.root
+    }
 }
--- app/src/main/java/com/virtuary/app/screens/manageAccount/ChangePasswordViewModel.kt
+++ app/src/main/java/com/virtuary/app/screens/manageAccount/ChangePasswordViewModel.kt
@@ -1,13 +1,68 @@
 package com.virtuary.app.screens.manageAccount
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class ChangePasswordViewModel : ViewModel() {
+    private val auth = FirebaseAuth.getInstance()
+    private val user = auth.currentUser
+
+    private val _invalidNewPassword = MutableLiveData<Boolean>()
+    val invalidNewPassword: LiveData<Boolean> = _invalidNewPassword
+
+    private val _invalidCurrentPassword = MutableLiveData<Boolean>()
+    val invalidCurrentPassword = _invalidCurrentPassword
+
+    private val _isSuccess = MutableLiveData<Boolean>()
+    val isSuccess = _isSuccess
+
+    private val _inProgress = MutableLiveData<Boolean>()
+    val inProgress = _inProgress
+
     val currentPassword = MutableLiveData("")
     val newPassword = MutableLiveData("")
 
     fun onClick() {
+        _invalidNewPassword.value = (newPassword.value?.length ?: 0) < 6
+        if (!_invalidNewPassword.value!!) {
+            _inProgress.value = true
+            verifyPassword { isVerified ->
+                _invalidCurrentPassword.value = !isVerified
+                if (isVerified) {
+                    viewModelScope.launch() {
+                        try {
+                            user?.updatePassword(newPassword.value!!)
+                            _isSuccess.value = true
+                        } catch (e: Exception) {
+                            _isSuccess.value = false
+                        }
+                    }
+                }
+                _inProgress.value = false
+            }
+        }
+    }
 
+    private fun verifyPassword(callback: (result: Boolean) -> Unit) {
+        if (currentPassword.value?.isNotEmpty()!!) {
+            val credential =
+                EmailAuthProvider.getCredential(user?.email ?: "", currentPassword.value!!)
+            viewModelScope.launch {
+                try {
+                    user?.reauthenticate(credential)?.await()
+                    callback(true)
+                } catch (e: Exception) {
+                    callback(false)
+                }
+            }
+        } else {
+            callback(false)
+        }
     }
 }
