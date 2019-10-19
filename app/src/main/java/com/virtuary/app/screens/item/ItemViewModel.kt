a58f73afbcdc349575bc0bf64da6832bfd19946c
Daryl Wong <daryl.jl.wong@gmail.com>
2019-10-20 02:15:01 +1100

    Implemented item deletion from index and visual overlay on trigger

--- app/src/main/java/com/virtuary/app/screens/item/ItemViewModel.kt
+++ app/src/main/java/com/virtuary/app/screens/item/ItemViewModel.kt
@@ -3,21 +3,30 @@ package com.virtuary.app.screens.item
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.algolia.search.model.ObjectID
 import com.google.firebase.storage.StorageReference
 import com.virtuary.app.firebase.FirestoreRepository
 import com.virtuary.app.firebase.Item
 import com.virtuary.app.firebase.StorageRepository
+import com.virtuary.app.util.AlgoliaClient
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 class ItemViewModel(item: Item) : ViewModel() {
     private val repository = FirestoreRepository()
     private val storageRepository = StorageRepository()
     private val _relatedTo = MutableLiveData<MutableList<String>>()
-    val relatedTo: LiveData<MutableList<String>>
-        get() = _relatedTo
+    val relatedTo: LiveData<MutableList<String>> = _relatedTo
 
     private val _itemImage = MutableLiveData<StorageReference>()
-    val itemImage: LiveData<StorageReference>
-        get() = _itemImage
+    val itemImage: LiveData<StorageReference> = _itemImage
+
+    private val _deleteTriggered = MutableLiveData(false)
+    val deleting: LiveData<Boolean> = _deleteTriggered
+
+    private val _deleteDone = MutableLiveData(false)
+    val deleteDone: LiveData<Boolean> = _deleteDone
 
     init {
         _relatedTo.value = mutableListOf()
@@ -29,6 +38,12 @@ class ItemViewModel(item: Item) : ViewModel() {
     }
 
     fun deleteItem(item: Item) {
-        repository.deleteItem(item)
+        _deleteTriggered.value = true
+        viewModelScope.launch() {
+            val index = AlgoliaClient.getIndex("item")
+            index.deleteObject(ObjectID(item.documentId!!))
+            repository.deleteItem(item).await()
+            _deleteDone.value = true
+        }
     }
 }
