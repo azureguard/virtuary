19a97c96ceae6b8d02f9e9f010752cbc769cf1e7
Maurice <Maurice_tikus@hotmail.com>
2019-10-19 17:01:51 +1100

    Reformatted the file

--- app/src/main/java/com/virtuary/app/MainActivityViewModel.kt
+++ app/src/main/java/com/virtuary/app/MainActivityViewModel.kt
@@ -3,7 +3,7 @@ package com.virtuary.app
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel : ViewModel() {
     val name = MutableLiveData("")
     val imageUploaded = MutableLiveData<String>()
 }
